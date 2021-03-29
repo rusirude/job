131cbb5b5cec862abc6f338bcb8cf95061e686b1
rusiru_d <rusiru_d@epiclanka.net>
2021-03-29 14:36:36 +0530

    login

--- src/main/java/com/leaf/job/JobApplication.java
+++ src/main/java/com/leaf/job/JobApplication.java
@@ -2,9 +2,15 @@ package com.leaf.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JobApplication {
+public class JobApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JobApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobApplication.class, args);
