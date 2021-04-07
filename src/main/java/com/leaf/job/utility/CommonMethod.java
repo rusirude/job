f955f522a12317d3b7904962de5b83b22077b1c0
rusiru_d <rusiru_d@epiclanka.net>
2021-04-07 07:53:29 +0530

    time zone fixed

--- src/main/java/com/leaf/job/utility/CommonMethod.java
+++ src/main/java/com/leaf/job/utility/CommonMethod.java
@@ -5,20 +5,36 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 @Component
 public class CommonMethod {
-    public Date getSystemDate() {
+    public Date getSystemDate2() {
         return new Date();
     }
 
-    public Date addHoursAndMinutesToDate(Date date,int hour,int minute) {
+    public Date getSystemDate() {
+       Calendar calendar = Calendar.getInstance();
+        TimeZone timeZone = TimeZone.getTimeZone(ZoneId.of(CommonConstant.DATE_TIMEZONE_JACKSON));
+        Calendar ret = new GregorianCalendar(timeZone);
+        ret.setTimeInMillis(calendar.getTimeInMillis() +
+                timeZone.getOffset(calendar.getTimeInMillis()) -
+                TimeZone.getDefault().getOffset(calendar.getTimeInMillis()));
+        return ret.getTime();
+    }
+
+    public Date addHoursAndMinutesToDate(Date date, int hour, int minute) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
-        calendar.add(Calendar.HOUR,hour);
-        calendar.add(Calendar.MINUTE,minute);
+        calendar.add(Calendar.HOUR, hour);
+        calendar.add(Calendar.MINUTE, minute);
         return calendar.getTime();
     }
 
